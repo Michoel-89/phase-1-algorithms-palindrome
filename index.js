9387a6c831b253747791413023779e865e4ff1b2
michoel <michoelr111@gmail.com>
2023-05-19 12:03:13 -0400

    complete

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    let indexOfWord = 0
+  for (let i = word.length -1; i >= 0; i--) {
+      const element = word[i];
+      if (element !==  word[indexOfWord]) {
+        return false
+      }
+      indexOfWord += 1
+  } return true
 }
 
 /* 
   Add your pseudocode here
+  I want to create the word backwards and then compare it to the current word
 */
 
 /*
   Add written explanation of your solution here
+  I start off by creating a variable which stores indexOfWord with an initial value of 0
+  then I loop through the word backwards and add 1 to the indexOfWord variable I then compare word[i] to word[indexOfWord]
+  if at any point they're different I return false if the entire loop executes I return true.
 */
 
 // You can run `node index.js` to view these console logs
